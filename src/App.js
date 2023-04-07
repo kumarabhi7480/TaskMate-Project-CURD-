ba293115ab46304d4f3b900500c6056c7431b634
Abhishek Kumar <kumarabhi7480>
2023-04-07 22:21:46 +0530

    TaskMate Project Using Locat Storage CURD

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
+import { useEffect, useState } from "react";
+import  Header  from "./components/Header";
+import  AddTask  from "./components/AddTask";
+import  ShowTask  from "./components/ShowTask";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from "react-bootstrap";
+
+
 import './App.css';
 
+
 function App() {
+  const [tasklist, setTasklist] = useState(JSON.parse(localStorage.getItem("tasklist")) || []);
+  const [task, setTask] = useState({});
+
+  useEffect(() => {
+    localStorage.setItem("tasklist", JSON.stringify(tasklist))
+  }, [tasklist]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+            <div className="App ">
+      <Header />
+      <AddTask 
+        tasklist={tasklist} 
+        setTasklist={setTasklist} 
+        task={task}
+        setTask={setTask}
+      />
+      <ShowTask 
+        tasklist={tasklist} 
+        setTasklist={setTasklist}
+        task={task}
+        setTask={setTask}  
+      />
+
     </div>
+      </Container>
   );
 }
 
